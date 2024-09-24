57b82c56712ca1cf9da520e9ca4a362261a3a032
yeonghui Hwang <nsun14363@gmail.com>
2024-09-24 17:18:36 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,11 +1,2 @@
 def solution(absolutes, signs):
-    # 절댓값 차례대로
-    # 실제 부호 boolean 차례대로
-    total = 0
-    for i in range(len(signs)):
-        if signs[i]:
-            total += absolutes[i]
-        else:
-            total -= absolutes[i]
-    
-    return total
+    return sum(absolute if sign else -absolute for absolute, sign in zip(absolutes,signs) )
