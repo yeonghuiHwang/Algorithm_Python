0cafd748c3a980e7494997deb23cf6d82e44632b
yeonghui Hwang <nsun14363@gmail.com>
2024-09-24 14:38:39 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,11 +1,2 @@
 def solution(n):
-    
-    list = []
-    while n > 0:
-        list.append(n%10)
-        n//=10
-        
-    return sum(list)
-        
-
-    
+    return sum(int(i) for i in str(n))
