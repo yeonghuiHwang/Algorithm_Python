6764c915df23f344aec8d640f6c67f72d0b19511
yeonghui Hwang <nsun14363@gmail.com>
2024-11-08 15:38:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 109.03 ms, Memory: 29.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,27 +1,53 @@
-import heapq as hq
+import heapq
 
 def solution(operations):
-    min_heap, max_heap = [],[]
-    
-    for op in operations:
-        op, value = op.split() # 문자열을 공백으로 분리
+    min_heap, max_heap = [], []
+    visited = [False] * 1000000  # 각 값의 유효성 체크를 위한 리스트
+    # 값이 삭제되면 False 부여하여 유효하지 않음을 표시
+    index = 0
+
+    for operation in operations:
+        op, value = operation.split()
         value = int(value)
-        
+
         if op == 'I':
-            hq.heappush(min_heap, value)
-            hq.heappush(max_heap, -value)
-        # 최댓값 삭제
+            heapq.heappush(min_heap, (value, index))
+            heapq.heappush(max_heap, (-value, index))
+            visited[index] = True
+            index += 1
+            
         elif op == 'D' and value == 1:
-            if max_heap :
-                max_value = -hq.heappop(max_heap) # 알아서 정렬
-                min_heap.remove(max_value) # 알아서 정렬해주지 않음
-                hq.heapify(min_heap)
+            # 최댓값 삭제
+            while max_heap and not visited[max_heap[0][1]]:
+                # 유효하지 않은 값들 삭제
+                heapq.heappop(max_heap)
+            if max_heap:
+                # 유효한 최댓값 제거
+                _, idx = heapq.heappop(max_heap) # -value, index
+                visited[idx] = False
         elif op == 'D' and value == -1:
-            if min_heap :
-                min_value = hq.heappop(min_heap)
-                max_heap.remove(-min_value)
-                hq.heapify(max_heap)
-                
-    if not min_heap: 
-        return [0,0]
-    return [-hq.heappop(max_heap), hq.heappop(min_heap)]
+            # 최솟값 삭제
+            while min_heap and not visited[min_heap[0][1]]:
+                # 유효하지 않은 값들 삭제
+                heapq.heappop(min_heap) 
+            if min_heap:
+                # 유효한 최솟값 제거
+                _, idx = heapq.heappop(min_heap) # value, index
+                visited[idx] = False
+
+    # 유효하지 않은 값 제거, 최종 값 보장
+    while min_heap and not visited[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+    while max_heap and not visited[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+
+    if not min_heap or not max_heap:
+        return [0, 0]
+    return [-max_heap[0][0], min_heap[0][0]]
+
+# 예제 입력 확인
+operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+print(solution(operations))  # 출력: [0, 0]
+
+operations = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
+print(solution(operations))  # 출력: [333, -45]
